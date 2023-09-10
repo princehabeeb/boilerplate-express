89e205c6fec5dd3a9d6ef3b0388689c7e91493cd
princehabeeb <habiy3400@gmail.com>
2023-09-11 00:11:43 +0100

    new

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let app = express();
-app.use("/public", express.static(__dirname + "/public"));
+// app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/views/index.html')
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/views/index.html')
+// });
+
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"});
 });
